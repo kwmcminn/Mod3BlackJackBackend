839418d55747382cabf89f152520e20e23477dff
github username <github email address>
2019-03-27 12:50:57 -0700

    added default for balance and changed users controller to find or create by

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::UsersController < ApplicationController
    end
 
    def create
-     @user = User.create(user_params)
+     @user = User.find_or_create_by(user_params)
      render json: @user
    end
 
@@ -24,7 +24,7 @@ class Api::V1::UsersController < ApplicationController
    private
 
      def user_params
-       params.permit(:username, :balance)
+       params.permit(:username)
      end
 
      def set_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
    has_many :hands 
+   after_initialize :init
+
+   def init 
+      self.balance = 1000
+      self.save
+   end 
 end
+
